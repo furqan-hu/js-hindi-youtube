ab23222c0efb3a940a8a6e6c0791fbb10b95a41a
furqan-hu <tasiyafurqan@gmail.com>
2025-11-15 08:46:27 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 2432;
+let accountEmail = "furqan@gmail.com";
+var accountPassword = "1234";
+accountCity = "Gujrat";
+
+
+// accountId = 23
+
+console.log(accountId);
+
+accountEmail = "faizan@gmail.com";
+accountPassword = "23344";
+accountCity = "Gujrat";
+let accountState;
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
