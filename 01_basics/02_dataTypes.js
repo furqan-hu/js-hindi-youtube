93a8a5b7f4dddad72f3bc4c5e3d53173af5e5408
furqan-hu <tasiyafurqan@gmail.com>
2025-11-15 08:46:50 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict" // treat all js code as newer version
+
+// alert(3+3); we are using node.js, not browser
+
+
+
+let name = "furqan";
+let age = 22;
+let isLoggedIn = false;
+let state;
+
+
+// number => 2 to the power 53
+// bigIng
+// string => ""
+// boolean => true/false
+// null => standluen value
+// undefined =>
+// symbol => unique
+
+
+// object
+// console.log(typeof state);
+console.log(typeof null);
